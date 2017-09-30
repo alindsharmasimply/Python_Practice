6ab5810feea90c2047cf57159b3f84ad53053aee
Alind Sharma <alindsharmasimply@gmail.com>
2017-09-30 06:00:05 +0530

    Almost Done

--- nintyFirst.py
+++ nintyFirst.py
@@ -4,33 +4,39 @@ root = Tk()
 
 L = []
 def file_submission_ADD():
-    L.append(entry1)
+    global L
+    L.append(display.get())
+    display.delete(0, END)
 
 def file_submission_SAVE():
+	global L
 	with open("text_submission_GUI.txt", "a+") as fp:
-		L.append(entry1)
+		L.append(display.get())
 		for i in L:
-			fp.write(i + '\n')
+			fp.write(i)
 		fp.close()
 	L = []
+	display.delete(0, END)
 			
 def file_submission_SAVE_and_CLOSE():
+	global L
 	with open("text_submission_GUI.txt", "a+") as fp:
-		L.append(entry1)
+		L.append(display.get())
 		for i in L:
-			fp.write(i + '\n')
-		fp.close()
+			fp.write(i)
+		fp.close() 
 	L = []
+	root.destroy()
 
-label1 = Button(root, text="Add File", COMMAND=file_submission_ADD)
-label2 = Button(root, text="Add and Save", COMMAND=file_submission_SAVE)
-label3 = Button(root, text="Save and Close", COMMAND=file_submission_SAVE_and_CLOSE)
-entry1 = Entry(root)
+label1 = Button(root, text="Add File", command=file_submission_ADD)
+label2 = Button(root, text="Add and Save", command=file_submission_SAVE)
+label3 = Button(root, text="Save and Close", command=file_submission_SAVE_and_CLOSE)
+display = Entry(root)
 
 label1.grid(row=0, column=1)
 label2.grid(row=0, column=2)
 label3.grid(row=0, column=3)
-entry1.grid(row=0, column=0)
+display.grid(row=0, column=0)
 
 
 root.mainloop()
