94cc9c231380c1177715378e2dccbd6b76c56bf9
Alind Sharma <alindsharmasimply@gmail.com>
2017-10-03 05:47:59 +0530

    This is my first commit on Day-33

---
+++ nintyEighth.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.click()
+
+pyautogui.typewrite('Hello World', interval=0.1)
+
+pyautogui.typewrite(['A', 'B', 'left', 'I'], interval=0.1)
+
+pyautogui.press('f1')
+
+pyautogui.hotkey('ctrl', 'o')
